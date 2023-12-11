8d52601e6b9ebdfb38ae6452c7388bee1527bf81
Yaroslav Turchenko <yaroslav.turchenko@tellurionmobile.com>
2023-12-11 09:47:07 +0200

    fixed algorithm

--- Assets/Scripts/EllersMaze.cs
+++ Assets/Scripts/EllersMaze.cs
@@ -1,115 +1,113 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class EllersMaze
-{
-    private Cell[,] _cells; // first bool true - right wall exist, second bool - down cell exist
-    private int _rows;
-    private int _cols;
-    private int[,] _maze;
-    private Dictionary<int, HashSet<int>> _sets;
-    
-    public EllersMaze(int rows, int cols)
+namespace DefaultNamespace
+{ 
+    public class EllersMaze : MonoBehaviour 
     {
-        _rows = rows;
-        _cols = cols;
-        _maze = new int[rows, cols];
-        _cells = new Cell[rows, cols];
+        
+    private int _cols;
+    private Dictionary<int, HashSet<int>> _sets;
+    private int[] _maze;
+    private bool[] _walls;
+
+    public EllersMaze(int size)
+    {
+        _cols = size;
+    }
+
+    public void Init()
+    {
+        _cols = 5;
+        _maze = new int[_cols + 2];
         _sets = new Dictionary<int, HashSet<int>>();
+        for (int col = 1; col <= _cols; col++)
+        {
+            _maze[col] = col;
+            _sets.Add(col, new HashSet<int> {col});
+        }
     }
     
-    public Cell[,] Generate()
+    public void GenerateRow()
     {
-        int currentRow = 0;
+        MakeHorizontalConnection();
         
-        for (int y = 0; y < _cells.GetLength(0); y++)
+        MakeVerticalConnection();
+    }
+
+    private void MakeHorizontalConnection()
+    {
+        for (int col = 1; col < _cols; col++)
         {
-            for (int x = 0; x < _cells.GetLength(1); x++)
+            if (_maze[col] != _maze[col + 1] && _sets[_maze[col]] != _sets[_maze[col + 1]] && Random.Range(0, 2) == 0)
             {
-                _cells[x, y] = new Cell(true, true);
+                JoinSets(_maze[col], _maze[col + 1]);
+                _maze[col + 1] = _maze[col];
+            }
+        }
+
+        foreach (var cell in _maze)
+        {
+            Debug.Log($"{cell}");
+        }
+    }
+    
+    private void MakeVerticalConnection()
+    {
+        
+        var maze2 = new int[_cols + 2];
+        var startIndex = 1;
+        Dictionary<int, HashSet<int>> sets2 = new Dictionary<int, HashSet<int>>();
+        foreach (var set in _sets)
+        {
+            List<int> list = new List<int>(set.Value);
+            var connections = Random.Range(1, list.Count + 1);
+
+            for (int i = 0; i < connections; i++)
+            {
+                var randIndex = Random.Range(0, list.Count);
+                int actualCellPosition = list[randIndex];
+                maze2[actualCellPosition] = _maze[actualCellPosition];
+
+                if (sets2.ContainsKey(maze2[actualCellPosition]))
+                    sets2[maze2[actualCellPosition]].Add(actualCellPosition);
+                else
+                {
+                    sets2.Add(maze2[actualCellPosition], new HashSet<int>(){actualCellPosition});
+                }
+            }
+        }
+
+        _sets = sets2;
+        _maze = maze2;
+
+        for (int i = 1; i <= _cols; i++)
+        {
+            if (_maze[i] == 0)
+            {
+                _maze[i] = _sets.Keys.Max() + 1;
+                _sets.Add(_maze[i], new HashSet<int> {i});
             }
         }
         
-        // Step 1: Initialize first row with unique sets
-        for (int col = 0; col < _cols; col++)
+        foreach (var cell in _maze)
         {
-            _maze[currentRow, col] = col + 1;
-            _sets[col + 1] = new HashSet<int> { col };
+            Debug.Log($"{cell}");
         }
-
-        while (currentRow < _rows - 1)
-        {
-            // Step 2: Randomly join adjacent cells, but they must not be in the same set
-            for (int col = 0; col < _cols - 1; col++)
-            {
-                bool shouldJoin = Random.Range(0, 2) == 0;
-
-                if (shouldJoin && _maze[currentRow, col] != _maze[currentRow, col + 1])
-                {
-                    JoinSets(_maze[currentRow, col], _maze[currentRow, col + 1]);
-                    _cells[currentRow, col].RightWall = false;
-                    _maze[currentRow, col + 1] = _maze[currentRow, col];
-                }
-            }
-
-            // Step 3: For each set, randomly create vertical connections for the next row
-            foreach (var set in _sets.Values.Distinct())
-            {
-                var columnsInSet = set.ToArray();
-                int connections = Random.Range(1, columnsInSet.Length + 1);
-
-                for (int i = 0; i < connections; i++)
-                {
-                    int randCol = columnsInSet[Random.Range(0, columnsInSet.Length)];
-                    _maze[currentRow + 1, randCol] = _maze[currentRow, randCol];
-                    _cells[currentRow, randCol].DownWall = false;
-                    //_sets[_maze[currentRow, randCol]].Add(randCol);
-                }
-            }
-
-            currentRow++;
-
-            // Step 4: For cells in the new row that have no set, give them a new set
-            for (int col = 0; col < _cols; col++)
-            {
-                if (_maze[currentRow, col] == 0)
-                {
-                    int newSetId = _sets.Keys.Max() + 1;
-                    _maze[currentRow, col] = newSetId;
-                    _sets[newSetId] = new HashSet<int> { col };
-                }
-            }
-        }
-
-        return _cells;
     }
 
     private void JoinSets(int setId1, int setId2)
     {
-        if(_sets.Keys.Contains(setId1) && _sets.Keys.Contains(setId2))
-            foreach (var col in _sets[setId2])
-            {
-                _sets[setId1].Add(col);
-            }
-
+        foreach (var col in _sets[setId2])
+        {
+            _sets[setId1].Add(col);
+            _maze[col] = setId1;
+        }
+        
         _sets.Remove(setId2);
     }
-}
-public struct Cell
-{
-    public bool RightWall { get; set; }
-    public bool DownWall { get; set; }
-    public int X { get; }
-    public int Y { get; }
-
-    public Cell(bool rightWall, bool downWall)
-    {
-        RightWall = rightWall;
-        DownWall = downWall;
-        X = 0;
-        Y = 0;
+    
     }
 }
--- Assets/Scripts/Generation/RoomGenerator.cs
+++ Assets/Scripts/Generation/RoomGenerator.cs
@@ -9,67 +9,17 @@ namespace Generation
     {
         public Tilemap tilemap; // Assign in inspector
         public Tile wallTile; // Assign your wall tile in inspector
-        private Cell[,] _maze;
-     
+
         private const int Rows = 100;
         private const int Cols = 100;
 
         private void Start()
         {
-            _maze = new EllersMaze(Rows, Cols).Generate();
             GenerateRoom();
         }
-
-
+        
         private void GenerateRoom()
         {
-            for (var x = 0; x < Rows; x++)
-            {
-                for (var y = 0; y < Cols; y++)
-                {
-                    Debug.Log($"Row {x}, Column {y}, Down wall{_maze[x, y].DownWall}, right wall:{_maze[x, y].RightWall}");
-                }
-            }
-
-            // Perimeter
-            for (var x = -1; x <= Rows * 2; x++)
-            {
-                for (var y = -1; y <= Cols * 2; y++)
-                {
-                    if (y == Cols * 2  || x == -1 || y == -1 || x == Rows * 2)
-                    {
-                        tilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
-                    }
-                }
-            }
-
-            for (var y = Cols * 2 - 2; y >= 0; y -= 2)
-            {
-                for (var x = 0; x < Rows * 2; x++)
-                {
-                    tilemap.SetTile(new Vector3Int(x, y), wallTile);
-                }
-            }
-
-            for (var row = Rows * 2 - 1; row > 0; row -= 2)
-            {
-                for (var col = 0; col < Cols * 2; col += 2)
-                {
-                    if (col == Rows * 2 - 2)
-                    {
-                        continue;
-                    }
-                    if (_maze[Mathf.Abs(row - Cols * 2) / 2, col / 2].RightWall)
-                    {
-                        tilemap.SetTile(new Vector3Int(col + 1, row), wallTile);
-                    }
-            
-                    if (!_maze[Mathf.Abs(row - Cols * 2) / 2, col / 2].DownWall)
-                    {
-                        tilemap.SetTile(new Vector3Int(col, row - 1), null);
-                    }
-                }
-            }
         }
     }
     
